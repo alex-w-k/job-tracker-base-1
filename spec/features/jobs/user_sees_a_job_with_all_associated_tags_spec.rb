ec18ba12553f7c81011f21fbc2301e739a470e82
Alex Koch <alex549us3@gmail.com>
2017-06-06 10:45:02 -0600

    better implementation, more dynamic, now for tags with multiple jobs

--- spec/features/jobs/user_sees_a_job_with_all_associated_tags_spec.rb
+++ spec/features/jobs/user_sees_a_job_with_all_associated_tags_spec.rb
@@ -14,4 +14,19 @@ describe 'User sees specific job with all associated tags' do
     expect(page).to have_content('70')
     expect(page).to have_content('Tags: Software - 1')
   end
+
+  scenario 'a user sees all tags for the job posting with multiple tags' do
+    company = Company.create(name: 'Test Company')
+    job = company.jobs.create(title: 'Developer', level_of_interest: 70, city: 'Denver')
+    tag1 = Tag.new(title: 'Software')
+    tag2 = Tag.new(title: 'Good-Location')
+    job.tags.append(tag1, tag2)
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content('Test Company')
+    expect(page).to have_content('Developer')
+    expect(page).to have_content('70')
+    expect(page).to have_content('Tags: Software - 1, Good-Location - 1')
+  end
 end
