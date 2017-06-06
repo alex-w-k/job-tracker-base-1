fb2a89672101643be871851d644096a01dde5c5d
Alex Koch <alex549us3@gmail.com>
2017-06-06 10:50:09 -0600

    test written and passing for tags with multiple jobs

--- spec/features/jobs/user_sees_a_job_with_all_associated_tags_spec.rb
+++ spec/features/jobs/user_sees_a_job_with_all_associated_tags_spec.rb
@@ -29,4 +29,31 @@ describe 'User sees specific job with all associated tags' do
     expect(page).to have_content('70')
     expect(page).to have_content('Tags: Software - 1, Good-Location - 1')
   end
+
+  scenario 'a user sees all tags with job count when tags are associated with multiple jobs' do
+    company = Company.create(name: 'Test Company')
+    job1 = company.jobs.create(title: 'Developer', level_of_interest: 70, city: 'Denver')
+    job2 = company.jobs.create(title: 'Mall Rat', level_of_interest: 10, city: 'Denver')
+    
+    tag1 = Tag.create(title: 'Software')
+    tag2 = Tag.create(title: 'Good-Location')
+    tag3 = Tag.create(title: 'Low-Paying')
+
+    job1.tags.append(tag1, tag2)
+    job2.tags.append(tag2, tag3)
+
+    visit company_job_path(company, job1)
+
+    expect(page).to have_content('Test Company')
+    expect(page).to have_content('Developer')
+    expect(page).to have_content('70')
+    expect(page).to have_content('Tags: Software - 1, Good-Location - 2')
+
+    visit company_job_path(company, job2)
+
+    expect(page).to have_content('Test Company')
+    expect(page).to have_content('Mall Rat')
+    expect(page).to have_content('10')
+    expect(page).to have_content('Tags: Good-Location - 2, Low-Paying - 1')
+  end
 end
